1f498ac3f2b6a7a9d2dde2cdeaa9831990c099d4
Y.Harsha Vardhan <2400040454@kluniversity.in>
2026-02-21 17:29:04 +0000

    Sync LeetCode submission - Valid Palindrome - Runtime - 3 ms (99.03%), Memory - 23.4 MB (8.12%)

---
+++ LeetCode/0125-valid-palindrome/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered = [ch.lower() for ch in s if ch.isalnum()]
+
+        left, right = 0, len(filtered)-1
+        while left < right:
+            if filtered[left] != filtered[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
