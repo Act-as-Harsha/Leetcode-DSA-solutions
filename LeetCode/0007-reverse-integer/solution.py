499f247f92ecd108b9df3ef8b070d1d262601e96
Y.Harsha Vardhan <2400040454@kluniversity.in>
2026-04-07 17:34:17 +0000

    Sync LeetCode submission - Reverse Integer - Runtime - 50 ms (31.36%), Memory - 19.2 MB (70.19%)

---
+++ LeetCode/0007-reverse-integer/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        rev = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        while x != 0:
+            digit = x % 10
+            x = x // 10
+            if rev > (2**31 - 1) // 10:
+                return 0
+
+            rev = rev * 10 + digit
+
+        return sign * rev
